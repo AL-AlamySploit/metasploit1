67848d9685b302f160487c771cff45347912eac7
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-08-29 04:02:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,29 +9,54 @@ echo "$A7Y╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚�
 echo
 echo
 echo "$A7Y[The code By With Metasploit]"
+echo
+center "*** Downloading..."
+cd $HOME
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1
 
-printf "${A7Y}[+] witing to Down..  ..\n"
-
-apt upgrade –y
-pkg install wget
-pkg install curl
-pkg install openssh
-pkg install git –y
-apt install ruby
-gem install bundle
+echo
+center "*** Installation..."
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
 gem install bundler
-pip install bundle
-php install bundler
-bundle install -j5
-bundle update
-bundle update nokogiri
-gem install nokogiri -- --use-system-libraries
-error nokogiri versi 1.8.2
-gem install nokogiri -v'1.8.0' -- --use-system-libraries
-gem update --system
-bundler update --bundler
-gem install bundler:1.17.3
-git clone https://github.com/rapid7/metasploit-framework.git
-ls
-cd metasploit-framework
-./msfconsole
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
+
+echo
+center "*** Database configuration..."
+cd $HOME/metasploit-framework/config
+curl -sLO https://raw.githubusercontent.com/AL-AlamySploit/rolling-repo/master/database.yml
+
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser msf
+createdb msf_database
+
+cd $HOME
+curl -sLO https://raw.githubusercontent.com/AL-AlamySploit/rolling-repo/master/postgresql_ctl.sh
+chmod +x postgresql_ctl.sh
+
+echo
+center "*"
+echo -e "\033[32m Installation complete. \n To start msf database use: ./postgresql_ctl.sh start \n Launch metasploit by executing: msfconsole\033[0m"
+center "*"
